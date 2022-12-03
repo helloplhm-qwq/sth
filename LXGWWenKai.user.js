adb1cfc9e2ba1503b94c9bce644b059aabd9ee56
彭狸花喵 <94218819+helloplhm-qwq@users.noreply.github.com>
2022-12-03 16:26:40 +0800

    Create LXGWWenKai.user.js

---
+++ LXGWWenKai.user.js
@@ -0,0 +1,19 @@
+/*
+ @match *
+ @name 无
+ @description 无
+ @author helloplhm-qwq
+ @version 0.1
+*/
+
+function loadStyle(url) {
+  var link = document.createElement('link')
+  link.type = 'text/css'
+  link.rel = 'stylesheet'
+  link.href = url
+  var head = document.getElementsByTagName('head')[0]
+  head.appendChild(link)
+}
+
+// 使用方法引入css
+loadStyle('https://jsd.cdn.zzko.cn/gh/helloplhm-qwq/sth@main/style.css')
